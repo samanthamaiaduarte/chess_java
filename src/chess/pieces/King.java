4cd033a92c62139cc0e2368dc1c2d7940dad2e42
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-03-21 20:46:16 -0300

    Implementing possible moves of King

--- src/chess/pieces/King.java
+++ src/chess/pieces/King.java
@@ -1,6 +1,7 @@
 package chess.pieces;
 
 import boardgame.Board;
+import boardgame.Position;
 import chess.ChessPiece;
 import chess.Color;
 
@@ -14,10 +15,66 @@ public class King extends ChessPiece {
 	public String toString() {
 		return "K";
 	}
+	
+	private boolean canMove(Position position) {
+		ChessPiece piece = (ChessPiece) getBoard().piece(position);
+		return piece == null || piece.getColor() != getColor();
+	}
 
 	@Override
 	public boolean[][] possibleMoves() {
 		boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		Position p = new Position(0, 0);
+		
+		//Searching above
+		p.setValues(position.getRow() - 1, position.getColumn());
+		if(getBoard().positionExists(p) && canMove(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+
+		//Searching left-above
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}		
+
+		//Searching left
+		p.setValues(position.getRow(), position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Searching left-below
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Searching below
+		p.setValues(position.getRow() + 1, position.getColumn());
+		if(getBoard().positionExists(p) && canMove(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+
+		//Searching below-right
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Searching right
+		p.setValues(position.getRow(), position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}		
+		
+		//Searching right-above
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		if(getBoard().positionExists(p) && canMove(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}		
+
 		return possibleMoves;
 	}
 }
