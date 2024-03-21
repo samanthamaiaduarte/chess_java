17e1b1c1228b6b4f652e02e94f0c7117017a8882
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-03-21 16:46:07 -0300

    Little improvement in board printing

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -30,7 +30,12 @@ public class ChessMatch {
 	
 	private void initialSetup () {
 		placeNewPiece('a', 8, new Rook(board, Color.WHITE));
+		placeNewPiece('h', 8, new Rook(board, Color.WHITE));
 		placeNewPiece('e', 8, new King(board, Color.WHITE));
+		
+		
+		placeNewPiece('a', 1, new Rook(board, Color.BLACK));
+		placeNewPiece('h', 1, new Rook(board, Color.BLACK));
 		placeNewPiece('e', 1, new King(board, Color.BLACK));
 	}
 }
