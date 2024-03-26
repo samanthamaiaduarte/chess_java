afe2ac994eff3a515db0b01d352299d254e1f943
samanthamaiaduarte <sam.maiaduarte@outlook.com>
2024-03-26 10:54:27 -0300

    Implementing Bishop

---
+++ src/chess/pieces/Bishop.java
@@ -0,0 +1,67 @@
+package chess.pieces;
+
+import boardgame.Board;
+import boardgame.Position;
+import chess.ChessPiece;
+import chess.Color;
+
+public class Bishop extends ChessPiece {
+
+	public Bishop(Board board, Color color) {
+		super(board, color);
+	}
+	
+	@Override
+	public String toString() {
+		return "B";
+	}
+
+	@Override
+	public boolean[][] possibleMoves() {
+		Position p = new Position(0, 0);
+		boolean[][] possibleMoves = new boolean[getBoard().getRows()][getBoard().getColumns()];
+		
+		//Searching above-left
+		p.setValues(position.getRow() - 1, position.getColumn() - 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() - 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p) ) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Searching below-left
+		p.setValues(position.getRow() + 1, position.getColumn() - 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() - 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p) ) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+
+		//Searching below-right
+		p.setValues(position.getRow() + 1, position.getColumn() + 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() + 1, p.getColumn() + 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p) ) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}
+		
+		//Searching above-right
+		p.setValues(position.getRow() - 1, position.getColumn() + 1);
+		while(getBoard().positionExists(p) && !getBoard().thereIsAPiece(p)) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+			p.setValues(p.getRow() - 1, p.getColumn() + 1);
+		}
+		if(getBoard().positionExists(p) && isThereOpponentPiece(p) ) {
+			possibleMoves[p.getRow()][p.getColumn()] = true;
+		}		
+
+		return possibleMoves;
+	}
+
+}
